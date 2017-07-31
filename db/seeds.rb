6b5c234039b86ab82974eebe996ebc6578636743
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-07-31 14:22:10 +0900

    seeds.rbのコメントアウト削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
-# 100.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#   name = Faker::StarWars.character
-#   User.create!(email: email,
-#                password: password,
-#                password_confirmation: password,
-#                name: name
-#                )
-# end
-# n = 1
-# while n <= 100
-#   title = Faker::Pokemon.name
-#   content = Faker::Lorem.paragraph
-#   Topic.create(
-#     title: title,
-#     content: content,
-#     user_id: n
-#   )
-#   n = n + 1
-# end
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::StarWars.character
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+end
+n = 1
+while n <= 100
+  title = Faker::Pokemon.name
+  content = Faker::Lorem.paragraph
+  Topic.create(
+    title: title,
+    content: content,
+    user_id: n
+  )
+  n = n + 1
+end
