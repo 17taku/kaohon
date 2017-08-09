86cb158e4377bf9034596138f2ee9e74e7b9956e
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-08-09 23:50:57 +0900

    sendgridのdefault_url_optionをAWSに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://still-falls-57741.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://13.114.0.75' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
+   domain: "",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
