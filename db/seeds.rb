fcc29a6ddd8a4566359ef438e83e437d73963222
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-08-11 21:50:12 +0900

    シードデータ3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,13 @@
-10.times do |n|
-  name = Faker::Pokemon.name
-  email = Faker::Internet.email
-  password = Faker::Internet.password
-  uid = SecureRandom.uuid
-  User.create( name: name,
-               email: email,
-               password: password,
-               uid: uid
-               )
-end
 
-n = 1
-while n <= 10
+10.times do |n|
   topic = Topic.new(
     content: 'トピック',
   user_id: n + 1)
 
-  i = 1
-  while i <= 10
+  10.times do
     topic.comments.build(
       content: 'コメント',
-    user_id: i + 1)
+    user_id: n + 1)
   end
 
   topic.save
