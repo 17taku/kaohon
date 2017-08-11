ec0bf39116849d4ce2f31f1d3f1bbf03bf3c9c6f
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-08-11 22:31:04 +0900

    seeds.rbにtitle追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  validates :title, presence: true
+  validates :content, presence: true
   belongs_to :user
   # CommentモデルのAssociationを設定
   has_many :comments, dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
+10.times do |n|
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  password = Faker::Internet.password
+  uid = SecureRandom.uuid
+  User.create( name: name,
+               email: email,
+               password: password,
+               uid: uid
+               )
+end
 
 10.times do |n|
   topic = Topic.new(
+    title: 'タイトル'
     content: 'トピック',
   user_id: n + 1)
 
